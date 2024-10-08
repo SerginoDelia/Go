8fc6043d842a5970d0ecefc2ef2d72424c82c12e
Sergino Delia <sergino.co@gmail.com>
2024-10-08 01:03:33 -0400

    added updates

---
+++ fizzbuzz.go
@@ -0,0 +1,11 @@
+// Fizzbuzz
+// If number is divisible by 3 print "Fizz"
+// If number is divisible by 5 print "Buzz"
+// If numebr is divisible by 3 and 5 print "FizzBuzz"
+
+package main
+
+import "fmt"
+
+func main() {}
+
