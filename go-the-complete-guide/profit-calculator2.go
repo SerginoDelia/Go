45de33ba7a9e535ed4c7279067a7c6dc8cd9a8ab
Sergino Delia <sergino.co@gmail.com>
2024-10-13 14:54:19 -0400

    added updates

--- go-the-complete-guide/profit-calculator2.go
+++ go-the-complete-guide/profit-calculator2.go
@@ -9,34 +9,56 @@ package main
 import (
   "fmt"
   "os"
-  //"strconv"
+  "errors"
 )
 
-func writeFinances(ebt, profit, ratio float64){
-  financesText := fmt.Sprintf("Earnings before tax %.2f:\nProfit: %.2f\nRatio: %.2f", ebt, profit, ratio )
-  os.WriteFile("finances.txt", []byte(financesText), 0644)
-}
-
 func main() {
-  revenue := getUserInput("Revenue: ") 
-  expenses := getUserInput("Expenses: ") 
-  taxRate := getUserInput("Tax Rate: ")
-  
+  revenue, err1 := getUserInput("Revenue: ")
+
+  if err1 != nil {
+    fmt.Println(err1)
+    return
+  }
+
+  expenses, err2 := getUserInput("Expenses: ")
+
+  if err2 != nil {
+    fmt.Println(err2)
+    return
+  }
+
+  taxRate, err3 := getUserInput("Tax Rate: ")
+ 
+  if err3 != nil {
+    fmt.Println(err3)
+    return
+  }
+
+  //if err1 != nil || err2 != nil || err3 != nil {
+    //fmt.Println(err1)
+    //return
+  //}  
+
   ebt, profit, ratio := calculateKpi(revenue, expenses, taxRate)
-  fmt.Sprintf("Earnings before tax: %.2f\nProfit: %.2f\nRatio: %.2f", ebt, profit, ratio)
+  fmt.Printf("Earnings before tax: %.2f\nProfit: %.2f\nRatio: %.2f\n", ebt, profit, ratio)
   
   //fmt.print("Earnings before tax: $", ebt)
   //fmt.Printf("Profit: %.2f\n", profit)
   //fmt.Printf("Ratio: %.3f\n", ratio)
 
-  writeFinances(ebt, profit, ratio)
+  storeResults(ebt, profit, ratio)
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
   var userInput float64
   fmt.Print(infoText)
   fmt.Scan(&userInput)
-  return userInput
+  
+  if userInput <= 0 {
+    return 0, errors.New("Value must be a positive number.")
+  }
+ 
+  return userInput, nil
 }
 
 
@@ -58,7 +80,10 @@ func calculateKpi(revenue, expenses, taxRate float64) (float64, float64, float64
   return ebt, profit, ratio
 }
 
-
+func storeResults(ebt, profit, ratio float64){
+  financesText := fmt.Sprintf("Earnings before tax %.2f:\nProfit: %.2f\nRatio: %.2f", ebt, profit, ratio )
+  os.WriteFile("results.txt", []byte(financesText), 0644)
+}
 
 
 
